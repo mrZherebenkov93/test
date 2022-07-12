48d72a49064af1e13479a5ff4b40bb41d501f218
mrZherebenkov93 <109137861+mrZherebenkov93@users.noreply.github.com>
2022-07-12 11:58:11 +0300

    Update and rename wp-content/themes/twentysixteen/functions.php to wp-контент/темы/двадцать шестьдесят/functions.php

--- wp-контент/темы/двадцать шестьдесят/functions.php
+++ wp-контент/темы/двадцать шестьдесят/functions.php
@@ -599,10 +599,8 @@ add_filter( 'widget_tag_cloud_args', 'twentysixteen_widget_tag_cloud_args' );
 
 add_action( 'init', 'create_taxonomy' );
 function create_taxonomy(){
-
-	// список параметров: wp-kama.ru/function/get_taxonomy_labels
 	register_taxonomy( 'realty', [ 'post' ], [
-		'label'                 => '', // определяется параметром $labels->name
+		'label'                 => '',
 		'labels'                => [
 			'name'              => 'Тип недвижимости',
 			'singular_name'     => 'Недвижимость',
